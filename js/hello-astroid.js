6d824faae26e502a57ac54281fa0571de921efc5
Emilio Benavides <emilio.benavides68@gmail.com>
2021-12-08 12:42:06 -0600

    added favorite food

--- js/hello-astroid.js
+++ js/hello-astroid.js
@@ -1,1 +1,5 @@
-console.log("my favorite food " + "a supreme pizza from " + "Lou's Wood Fire Pizza.")
+console.log("my favorite food " + "\n\ta supreme \n\t\t\tpizza from " + "Lou's \n\tWood Fire \n\t\t\tPizza \n\t they give\n\t\t\t pregnant \n\twoman \n\t\t\t10% off.")
+
+
+var favoriteFood = "hamburgers"
+console.log(favoriteFood + "!")
