b00400df374e0ef666278bb178fe4daad90c2124
Emilio Benavides <emilio.benavides68@gmail.com>
2021-12-10 15:11:11 -0600

    completed color exercise

---
+++ js/external.js
@@ -0,0 +1,8 @@
+console.log("Hello from external JavaScript!");
+
+alert("Welcome to my Website!");
+
+
+var favoriteColor = prompt("what is your favorite color?");
+
+alert("Great, " + favoriteColor + " is my favorite color too!");
