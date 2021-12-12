8a37bd5918db82758220b7579cdd4ad984e13be7
Emilio Benavides <emilio.benavides68@gmail.com>
2021-12-12 12:02:32 -0600

    Completed movie alert exercise

--- js/external.js
+++ js/external.js
@@ -1,8 +1,17 @@
-console.log("Hello from external JavaScript!");
+//console.log("Hello from external JavaScript!");
 
-alert("Welcome to my Website!");
+//alert("Welcome to my Website!");
 
 
-var favoriteColor = prompt("what is your favorite color?");
+//var favoriteColor = prompt("What is your favorite color?");
 
-alert("Great, " + favoriteColor + " is my favorite color too!");
+//alert("Great, " + favoriteColor + " is my favorite color too!");
+
+
+
+var littleMermaid = prompt("How long did you keep this the Little Mermaid?");
+var brotherBear = prompt("how long did you keep the movie Brotherbear?");
+var hercules = prompt("how long did you rent the movie Hercules?");
+var totalAmount = parseInt(littleMermaid) + parseInt(brotherBear) + parseInt(hercules);
+
+alert("Your total is $" + (totalAmount * 3));
