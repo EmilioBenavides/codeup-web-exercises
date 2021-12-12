75f3de128d3aa218035d6696c687af9fee521d3e
Emilio Benavides <emilio.benavides68@gmail.com>
2021-12-12 13:20:44 -0600

    Completed Amazon, Facebook, Google pay example.

--- js/external.js
+++ js/external.js
@@ -9,9 +9,17 @@
 
 
 
-var littleMermaid = prompt("How long did you keep this the Little Mermaid?");
-var brotherBear = prompt("how long did you keep the movie Brotherbear?");
-var hercules = prompt("how long did you rent the movie Hercules?");
-var totalAmount = parseInt(littleMermaid) + parseInt(brotherBear) + parseInt(hercules);
+//var littleMermaid = prompt("How long did you keep this the Little Mermaid?");
+//var brotherBear = prompt("how long did you keep the movie Brotherbear?");
+//var hercules = prompt("how long did you rent the movie Hercules?");
+//var totalAmount = parseInt(littleMermaid) + parseInt(brotherBear) + parseInt(hercules);
 
-alert("Your total is $" + (totalAmount * 3));
+//alert("Your total is $" + (totalAmount * 3));
+
+
+var google = prompt("How many hours did you work at Google?");
+var amazon =  prompt("How many hours did you work at Amazon?");
+var facebook = prompt("How many hours did you work at Facebook?");
+var totalPayout = parseInt(google * 400) + parseInt(amazon * 380) + parseInt(facebook * 350);
+alert("Your total pay for the week is $" + (totalPayout));
+
