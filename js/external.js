6b12a3d2af77502a090c35818c99378200c4b0ca
Emilio Benavides <emilio.benavides68@gmail.com>
2021-12-12 21:03:43 -0600

    Completed product offer exercise.

--- js/external.js
+++ js/external.js
@@ -24,13 +24,20 @@
 //alert("Your total pay for the week is $" + (totalPayout));
 
 
-var classIsNotFull = prompt('This class in not full. Would you like to join?');
+// var classIsNotFull = prompt('This class in not full. Would you like to join?');
+//
+// let noScheduleConflicts = null;
+// if (classIsNotFull === 'yes') {
+//     noScheduleConflicts = prompt('This class is available these days, does that work for you?');
+// }
+//
+// if (classIsNotFull === 'no') alert('Thank you! Have a good day!');
+// if (classIsNotFull === 'yes' && noScheduleConflicts === 'no') alert('Thank you! Have a good day!');
+// if (classIsNotFull === 'yes' && noScheduleConflicts === 'yes') alert('Congratulations you are now enrolled');
 
-let noScheduleConflicts = null;
-if (classIsNotFull === 'yes') {
-    noScheduleConflicts = prompt('This class is available these days, does that work for you?');
-}
+var premiumMember = prompt("Are you a premium member?");
+var productAmount = prompt("How many items do you have?");
+var offerValid = prompt("Is your offer valid?");
+if (offerValid === 'no' || (productAmount < 2 && offerValid === 'yes') || (premiumMember === 'no' && offerValid === 'yes')) alert("Discount cannot be applied");
+if (offerValid === 'yes' && (productAmount > 2 || premiumMember === 'yes')) alert("Discount has been applied");
 
-if (classIsNotFull === 'no') alert('Thank you! Have a good day!');
-if (classIsNotFull === 'yes' && noScheduleConflicts === 'no') alert('Thank you! Have a good day!');
-if (classIsNotFull === 'yes' && noScheduleConflicts === 'yes') alert('Congratulations you are now enrolled');
