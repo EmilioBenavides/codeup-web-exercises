d2e38b39fc2e1d4ba7a7b710c338e8a5db1857bd
Emilio Benavides <emilio.benavides68@gmail.com>
2022-01-06 10:54:46 -0600

    Started objects exercise

---
+++ js/objects.js
@@ -0,0 +1,27 @@
+"use strict";
+
+
+//* MINI EXERCISE 10 minutes ***
+// Declare an array of two objects for movies
+// that have the following member values:
+// title (string)
+// year released (int)
+// rating (string)
+// director (string)
+// imdb rating (decimal)
+// academy award win (boolean)
+// Use imdb.com to look up the info or make up your own info
+// write a printMovie function that takes a movie as a parameter and prints its info.
+// call it with each of your array elements.
+// write a printMovies function that takes an array as a parameter, iterates over the array, and calls printMovie on each of the array elements (edited)
+
+var favMovie = {};
+
+favMovie.title = "Willow";
+favMovie.yearreleased = 1988;
+favMovie.rating = "The movie has a " + 7.3 + " rating";
+favMovie.director = "Ron Howard";
+favMovie.imdbrating = 7.3;
+favMovie.academyawardwin : false
+
+
