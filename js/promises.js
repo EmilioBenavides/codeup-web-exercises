944d5e48e351fc66293c7871d7e149bfbb443b86
Emilio Benavides <emilio.benavides68@gmail.com>
2022-03-14 21:34:52 -0500

    Completed promises exercise

--- js/promises.js
+++ js/promises.js
@@ -1,4 +1,30 @@
+// const myPromise = fetch(url, {headers: { EMILIO_GITHUB_TOKEN}});
+const formatDate = function (date) {
+    return new Date(date).toLocaleDateString()
+}
+const commitHistory = function (username) {
+    fetch(`https://api.github.com/users/${username}/events`, {headers: {Authorization: `token ${EMILIO_GITHUB_TOKEN}`}})
+        .then(function (re) {
+            return re.json()
+        }).then(function (results) {
+        for (let result of results) {
+            if (result.type === 'PushEvent') {
+                console.log(formatDate(result.created_at))
+                return result.created_at;
+            }
+        }
+    })
 
-const myPromise = fetch(url, {headers: { EMILIO_GITHUB_TOKEN}});
-const commitHistory = fetch(`https://api.github.com/users/${username}/events`)
+}
+commitHistory("EmilioBenavides")
 
+
+const wait = function (time) {
+    new Promise((resolve, reject) => {
+        setTimeout(function () {
+            resolve(time);
+        },time);
+    });
+}
+wait(1000).then(r => console.log(`You'll see this after ${r/1000} seconds`));
+wait(3000).then(r => console.log(`You'll see this after ${r/1000} seconds`));
